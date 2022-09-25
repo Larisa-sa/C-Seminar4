8d076637b97f2c96dc84485d53159824e6d4488a
Larisa <larisamash01@gmail.com>
2022-09-26 00:06:44 +0300

    1 задача домашки

---
+++ homework1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+Console.Clear();
+Console.WriteLine("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine($"{a}, {b} -> {step}");
