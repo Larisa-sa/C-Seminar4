fb6d5d463825822fb5674e1a49d89d50238934b9
Larisa <larisamash01@gmail.com>
2022-09-26 00:23:57 +0300

    2 задача домашки

---
+++ homework2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear(); 
+Console.Write("Введите число: "); // вводим по условию число
+int number = int.Parse(Console.ReadLine()); // переводим в int
+
+int GetSum(int number) // метод
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int num = number % 10;
+        number = number / 10;
+        sum = sum + num;
+    }
+return sum;
+}
+Console.WriteLine($"{number} -> {GetSum(number)}");
